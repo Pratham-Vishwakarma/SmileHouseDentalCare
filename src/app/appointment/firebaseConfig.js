4acf0aa13d71dab7b50097b81f1cc9b91698eee7
Pratham-Vishwakarma <147902380+Pratham-Vishwakarma@users.noreply.github.com>
2024-12-28 06:51:53 +0530

    Enhanced Security.

--- src/app/appointment/firebaseConfig.js
+++ src/app/appointment/firebaseConfig.js
@@ -7,13 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA--Ca2bO-A5mkP0Lsn0JWz5jkGqrKv7OA",
-  authDomain: "doctor-ratnesh-smilehouse.firebaseapp.com",
-  projectId: "doctor-ratnesh-smilehouse",
-  storageBucket: "doctor-ratnesh-smilehouse.firebasestorage.app",
-  messagingSenderId: "1014009742205",
-  appId: "1:1014009742205:web:1103f6a43f2e996b3a8214",
-  measurementId: "G-3DJZ7BDSVC"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
